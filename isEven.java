f72bbc4cdae79fcaa6d1cbf5dc815cdbd5a6fade
Bhaavya Tilhari <bhaavyatilhari3498@gmail.com>
2026-06-07 18:18:20 +0530

    Add files via upload

---
+++ isEven.java
@@ -0,0 +1,5 @@
+//Code to read an integer and determine whether it is even or not. Returns 2 if even and 1 if not.
+
+public int isEven (int input1) {
+	return (input1 % 2 == 0) ? 2 : 1;
+}
---
+++ isOdd.java
@@ -0,0 +1,5 @@
+// Code to read an integer and determine whether it is odd or not. Returns 2 if odd and 1 if not.
+
+public int isOdd (int input1) {
+	return (input1 % 2 != 0) ? 2 : 1;
+}
